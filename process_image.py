be69eb267fe010155a36af0ee107a21ebd7a6f33
cmwenliu <cmwenliu@gmail.com>
2018-07-24 15:58:09 -0500

    .

--- process_image.py
+++ process_image.py
@@ -5,11 +5,15 @@ import os.path
 
 def rescale(img):
     newimg = cv2.resize(img,None,fx=1.2, fy=1.2, interpolation = cv2.INTER_LINEAR)
-    #cv2.imwrite( "new.png", img )
+    #towt = os.path.join('/img-transfer', 're.jpg')    
+    cv2.imwrite('re.jpg', newimg)
+
     return newimg
 
 def binarize(img):
     newimg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    #towt = os.path.join('/img-transfer', 'gray.jpg')
+    cv2.imwrite('gray.jpg', newimg)
     return newimg
 
 def remove_noise(img):
@@ -19,8 +23,10 @@ def remove_noise(img):
 
 def improve(input_file):
     # Load the image
-    orig_img = cv2.imread(input_file)
-    output_file = binarize(orig_img)
+    img = cv2.imread(input_file)
+    #towt = os.path.join('/img-transfer', 'ori.jpg')
+    cv2.imwrite('ori.jpg', img)
+    img = rescale(img)
+    img = binarize(img)
     #output_file = remove_noise(output_file)
     #output_file = rescale(output_file)
-    return output_file
